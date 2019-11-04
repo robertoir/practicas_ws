506f361d470b1cee6702244a25ebd339021350d5
robertoir <roberto.iglesias.rodriguez@usc.es>
2019-11-04 02:31:22 +0100

    sesion 2 proyecto_integrado

---
+++ escucha_arduino_1/scripts/escucha.py
@@ -0,0 +1,18 @@
+﻿#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+
+def callback(msg):
+	rospy.loginfo (msg.data)
+
+def subscriber():
+	rospy.init_node(‘suscriptor_de_roberto’,anonymous=True)
+	sub=rospy.Subscriber(‘/chatter’,String,callback)
+	rospy.spin()
+
+if __name__==’main’:
+	try:
+		publisher()
+	except rospy.ROSInterruptException:
+		pass
